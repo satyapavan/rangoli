5098bfbf3ca8aafb6662c0be6fc689a061757ac2
Satya Pavan <pavan.simham@gmail.com>
2018-03-02 00:34:53 +0530

    tied adding slider

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 
+var Hypotrochoid = function(R, r, d) {
+  console.log ( R,r,d )
+  
+  // officially a hypotrochoid has R > r > d (in the analog world)
+  this.R = R;
+  this.r = r;
+  this.d = d;
+
+  this.init();
+}
 
 function startRangoli() {
 	console.log("Entering into startRangoli");
